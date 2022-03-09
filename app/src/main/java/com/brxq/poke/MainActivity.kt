b6eb73ce4d52d9fc850b233840a1f29254f37863
rogozinnickolay0 <rogozinnickolay0@gmail.com>
2022-03-09 13:07:00 +0300

    graddle working empty directory

---
+++ app/src/main/java/com/brxq/poke/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.brxq.poke
+
+import android.app.Application
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.brxq.poke.ui.theme.PokeTheme
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var text : String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PokeTheme {
+                Text(text)
+            }
+        }
+    }
+}
+
+@HiltAndroidApp
+class MyApp : Application(){}
+
+@Module
+@InstallIn(SingletonComponent::class)
+class MyModule{
+
+    @Provides
+    @Singleton
+    fun provideString() = "Provided String"
+
+}
+
+
+
